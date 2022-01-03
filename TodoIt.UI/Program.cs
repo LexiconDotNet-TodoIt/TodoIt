285692cafca2fafe4149267af80211f82a96e3d8
GAMO <38156503+gamo-code@users.noreply.github.com>
2022-01-03 13:24:06 +0100

    Init

---
+++ TodoIt.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoIt.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
