786f4d734dcd33577f192194dccb36c85c93fc73
Zealot0150 <per.claeson@gmail.com>
2022-01-04 07:07:27 +0100

    PersonSeqvencer added TestSeqvencer added

---
+++ TodoIt.Test/TestSeqvencer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using TodoIt;
+
+namespace TodoIt.Test
+{
+
+    public class TestSeqvencer
+    {
+        [Fact]
+        public void TestPersonId()
+        {
+            int result = 0;
+            int id = Data.PersonSeqvencer.NextPersonId();
+            Assert.Equal(id, result);
+
+            id = Data.PersonSeqvencer.NextPersonId();
+            result = 1;
+            Assert.Equal(id, result);
+        }
+
+        [Fact]
+        public void TestReset()
+        {
+            int result = 0;
+            int id;
+
+            Data.PersonSeqvencer.Reset();
+            id = Data.PersonSeqvencer.NextPersonId();
+
+            Assert.Equal(id, result);
+        }
+    }
+}
---
+++ TodoIt/Data/PersonSeqvencer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace TodoIt.Data
+{
+	public class PersonSeqvencer
+	{
+		private static int personId = 0;
+
+		public static int NextPersonId()
+        {
+			return personId++;
+        }
+
+		public static void Reset()
+		{ 
+			personId = 0;
+		}
+	}
+}
+
