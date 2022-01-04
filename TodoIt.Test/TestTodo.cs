da4e815c0ba62c3842d158b215c71cf8c6a41bae
Thomas Selin <selin.reso@gmail.com>
2022-01-04 08:55:36 +0100

    Create Todo class in the model folder

---
+++ TodoIt.Test/TestTodo.cs
@@ -0,0 +1,25 @@
+﻿using TodoIt.Model;
+using Xunit;
+
+namespace TodoIt.Test
+{
+    public class TestTodo
+    {
+        [Theory]
+        [InlineData(0, "Description one", true)]
+        [InlineData(1, "Description two", true)]
+        public void TestGetSetProperties(
+            int id, string description, bool done)
+        {
+            Todo todo = new Todo(id, description);
+            todo.Done = done;
+            todo.Assignee = new Person(0, "Chuck", "Norris");
+
+            Assert.Equal(id, todo.TodoId);
+            Assert.NotNull(todo.Decription);
+            Assert.Equal(description, todo.Decription);
+            Assert.True(todo.Done);
+            Assert.NotNull(todo.Assignee);
+        }
+    }
+}
