748e7c7bbb447dfc89fa140e47a1853d096bba95
Thomas Selin <selin.reso@gmail.com>
2022-01-04 12:36:30 +0100

    Create People class

---
+++ TodoIt.Test/TestPeople.cs
@@ -0,0 +1,65 @@
+﻿using Xunit;
+
+namespace TodoIt.Test
+{
+    public class TestPeople
+    {
+        private readonly People people;
+
+        public TestPeople()
+        {
+            PersonSequencer.Reset();
+            people = new People();
+        }
+
+        private void AddPersonsToPeople(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                people.CreatePerson($"FirstName{i}", $"LastName{i}");
+            }
+        }
+
+        [Fact]
+        public void TestPeopleInitializedToZeroLength()
+        {
+            Assert.Empty(people.FindAll());
+        }
+
+        [Theory]
+        [InlineData(10)]
+        public void TestPeopleSize(int iterations)
+        {
+            AddPersonsToPeople(iterations);
+
+            Assert.Equal(iterations, people.Size());
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void TestPeopleFindById(int iterations)
+        {
+            AddPersonsToPeople(iterations);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                Assert.Equal($"FirstName{i}", people.FindById(i).FirstName);
+                Assert.Equal($"LastName{i}", people.FindById(i).LastName);
+            }
+        }
+
+        [Theory]
+        [InlineData(2)]
+        public void TestPeopleCreatePerson(int iterations)
+        {
+            AddPersonsToPeople(iterations);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                Assert.Equal($"FirstName{i}", people.FindAll()[i].FirstName);
+                Assert.Equal($"LastName{i}", people.FindAll()[i].LastName);
+            }
+        }
+
+    }
+}
---
+++ TodoIt/Data/People.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace TodoIt
+{
+    public class People
+    {
+        private static Person[] people;
+
+        public People()
+        {
+            this.Clear();
+        }
+
+        public int Size()
+        {
+            return people.Length;
+        }
+
+        public Person[] FindAll()
+        {
+            return people;
+        }
+
+        public Person FindById(int personId)
+        {
+            Person personToFind = null;
+
+            foreach (var person in people)
+            {
+                if (person.PersonId == personId)
+                {
+                    personToFind = person;
+                }
+            }
+
+            return personToFind;
+        }
+
+        public Person CreatePerson(string firstName, string lastName)
+        {
+            Person personToCreate = new Person(
+                PersonSequencer.NextPersonId(),
+                firstName, 
+                lastName);
+
+            Array.Resize(ref people, Size() + 1);
+
+            people[Size() - 1] = personToCreate;
+
+            return personToCreate;
+
+        }
+
+        public void Clear()
+        {
+            people = new Person[0];
+        }
+    }
+}
