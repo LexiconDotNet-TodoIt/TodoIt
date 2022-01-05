b88457a9bd5d2682986a920ba55cef67eae34dc6
Thomas Selin <selin.reso@gmail.com>
2022-01-05 12:51:35 +0100

    Rewrite people remove method

--- TodoIt/Data/People.cs
+++ TodoIt/Data/People.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace TodoIt
 {
@@ -49,7 +48,39 @@ namespace TodoIt
 
         public void RemovePerson(Person personToRemove)
         {
-            people = people.Where(person => person != personToRemove).ToArray();
+            if (FindById(personToRemove.PersonId) != null)
+            {
+                int peopleIndexToRemove = 0;
+                Person[] newPeopleArray = new Person[0];
+
+                for (int i = 0; i < Size(); i++)
+                {
+                    if (people[i] == personToRemove)
+                    {
+                        peopleIndexToRemove = i;
+                    }
+                }
+
+                for (int i = 0; i < Size(); i++)
+                {
+                    if (i != peopleIndexToRemove)
+                    {
+                        newPeopleArray.Append(people[i]).ToArray();
+                    }
+                }
+
+                people = newPeopleArray;
+            }
+
+
+            /*
+             Add the following to TodoItems AND People class.
+a. Functionality to remove object from array. (not nulling)
+First: you need to find the correct array index of the object.
+Second: You need to rebuild array by excluding the object on found index.
+b. Unit test changes
+c. Commit and Push to GitHub
+             */
         }
     }
 }
